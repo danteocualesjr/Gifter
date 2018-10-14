c00941ed35bbf634042ff76c71afab4632038862
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-14 23:37:17 +0800

    Added a comment in App.js.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Refactored class component to functional component.
 const App = () => {
   return(
     <div>Hey, how you doin'?</div>
