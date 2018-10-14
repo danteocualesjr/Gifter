979e459684f2b41d8f6abc67bb3f85f47a19c7dc
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-14 23:42:32 +0800

    Created a basic functional component for LoginPage.

---
+++ src/components/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h2>Simple Login Page Here</h2>
+        </div>
+    )
+};
+
+export default LoginPage;
