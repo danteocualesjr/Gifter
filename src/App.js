71ae145bdf8ff55972d538f8d65cfc566ea939b7
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-15 09:51:12 +0800

    In App.js, added route to LoginPage.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
 const App = () => {
   return(
     <div>
       <Route path="/" exact component={HomePage} />
+      <Route path="/login" exact component={LoginPage} />
     </div>
   )
 }
