3d3c9f5b6c9316bbb6f1b801724a1c40a40ec6b5
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-17 09:33:51 +0800

    Configured LoginForm.

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -3,14 +3,28 @@ import { Form, Button } from 'semantic-ui-react';
 
 class LoginForm extends from Component {
     state = {
-        data: {},
+        data: {
+            email: '',
+            password: ''
+        },
         loading: false,
         errors: {}
     };
 
     render() {
+        const { data } = this.state;
         return (
             <Form>
+                <Form.Field>
+                    <label htmlFor='email'>Email</label>
+                    <input 
+                        type='email' 
+                        id='email' 
+                        name='email' 
+                        placeholder='example@example.com' />
+                        value={data.email}
+                        onChange={this.onChange}
+                </Form.Field>
                 <Button primary>Login</Button>
             </Form>
         );
