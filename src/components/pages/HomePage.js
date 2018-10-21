8f49c2ea53f2d9a299b8c39c4a478c9c233a3fa9
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-21 16:08:23 +0800

    Added basic styling to HomePage.

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const HomePage = () => {
     return (
-        <div>
+        <div className="div">
             <h1>Gifter Home Page</h1>
             <Link to="/login">Login</Link>
         </div>
