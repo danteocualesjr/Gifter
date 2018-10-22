59bfc0ad76cc448872880976893dcd4a82c66579
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-22 17:27:20 +0800

    Edited HomePage.

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
     return (
         <div className="div">
-            <h1>Gifter</h1>
-            <h2>A quick and easy way to send gifts to children online.</h2>
-            <Link to="/login">Click here to log in.</Link>
+            <h1>Welcome to Gifter</h1>
+            <h2>Gifter provides a quick and easy way to send gifts to children online.</h2>
+            <Link to="/login">Click here to log in</Link>
         </div>
     )
 }
