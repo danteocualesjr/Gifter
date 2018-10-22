ed283041b48037ca242b1337edeeed58e36794c7
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-22 16:04:53 +0800

    Added text to HomePage.

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,6 +5,7 @@ const HomePage = () => {
     return (
         <div className="div">
             <h1>Gifter Home Page</h1>
+            <h2>A quick and easy way to send gifts to children online.</h2>
             <Link to="/login">Login</Link>
         </div>
     )
