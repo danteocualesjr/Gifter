9871d6ca21fce2586e0033ae6bc73e61554d49b5
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-24 17:55:19 +0800

    In LoginPage, refactored functional component to class component.

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import LoginForm from '../forms/LoginForm';
 
-const LoginPage = () => {
-    return (
-        <div className="div">
-            <h1>Gifter Login Page</h1>
+class LoginPage extends Component {
+    render() {
+        return (
+            <div className="div">
+                <h1>Gifter Login Page</h1>
             
-            <LoginForm submit={this.submit} />
-        </div>
-    )
-};
+                <LoginForm submit={this.submit} />
+            </div>
+        )
+    }
+}
 
 export default LoginPage;
