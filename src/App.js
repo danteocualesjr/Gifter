a433415700746e2cc2fd237a951be233406ec87f
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-07 05:28:53 +0800

    Added route to DashboardPage.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
+import DashboardPage from './components/pages/DashboardPage';
 
 const App = () => {
   return(
     <div className="ui container">
       <Route path="/" exact component={HomePage} />
       <Route path="/login" exact component={LoginPage} />
-      {/* Add more routes here */}
+      <Route path="/dashboard" exact component={DashboardPage} />
     </div>
   )
 }
