a054f1b82f830f942b6b0b5ec6e260ab0a92698c
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-07 04:58:03 +0800

    Configured HomePage.

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import * as actions from '../../actions/auth';
 
-const HomePage = ({ isAuthenticated }) => {
+const HomePage = ({ isAuthenticated, logout }) => {
     return (
         <div className="div">
             <h1>Welcome to Gifter</h1>
             <h2>Gifter provides a quick and easy way to send gifts to children online.</h2>
-            {isAuthenticated ? <button>Logout</button> : <Link to="/login">Login</Link>}
+            {isAuthenticated ? <button onClick={() => logout}>Logout</button> : <Link to="/login">Login</Link>}
         </div>
     )
 };
@@ -23,4 +24,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps)(HomePage);
+export default connect(mapStateToProps, { logout: actions.logout })(HomePage);
