0ef0a90b3a754cd5f52596a127471cf17235d601
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-08 14:35:42 +0800

    Imported PropTypes into App.js.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
