78121ec5cf40163290d2abfd1d61c91a6fdc18ab
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-08 13:35:23 +0800

    Created ConfirmEmailMessage in messages folder.

---
+++ src/components/messages/ConfirmEmailMessage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ConfirmEmailMessage = () => {
+    return (
+        <Message info>
+            <Message.Header>
+                Please confirm your email.
+            </Message.Header>
+        </Message>
+    )
+}
+
+export default ConfirmEmailMessage;
