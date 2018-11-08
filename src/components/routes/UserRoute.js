29c3f455de9b150c661eb906948070208034250b
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-08 13:59:20 +0800

    Configured UserRoute.

--- src/components/routes/UserRoute.js
+++ src/components/routes/UserRoute.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Route } from 'react-router-dom';
 
 const UserRoute = ({ component: Component, ...rest }) => {
     return (
         <Route {...rest} render={props => <Component {...props} />} />
     )
+};
+
+UserRoute.propTypes = {
+    component: PropTypes.func.isRequired
 };
 
 export default UserRoute;
