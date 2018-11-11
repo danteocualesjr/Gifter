c4b7a2856263d13be7f1043fbea8e54585b12146
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-12 05:11:27 +0800

    Configured api.js and HomePage.js.

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -6,13 +6,16 @@ import * as actions from '../../actions/auth';
 
 const HomePage = ({ isAuthenticated, logout }) => {
     return (
-        <div className="div">
+        <div>
             <h1>Welcome to Gifter</h1>
             <h2>Gifter provides a quick and easy way to send gifts to children online.</h2>
-            {isAuthenticated ? <button onClick={() => logout}>Logout</button> : <Link to="/login">Login</Link>}
+            {isAuthenticated ? (
+                <button onClick={() => logout()}>Logout</button>
+            ) : ( 
+            <div><Link to="/login">Login</Link> or <Link to="/signup">Sign Up</div>
+            )}
         </div>
     )
-};
 
 HomePage.propTypes = {
     isAuthenticated: PropTypes.bool.isRequired,
