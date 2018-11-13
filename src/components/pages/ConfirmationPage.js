c3118f8178cfe7c0ff3ab6522d6167d9bd4b2985
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-13 14:23:33 +0800

    Configured ConfirmationPage.

--- src/components/pages/ConfirmationPage.js
+++ src/components/pages/ConfirmationPage.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import { Message, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 class ConfirmationPage extends Component {
     state = {
         loading: true,
         success: false
-    }
+    };
+
+    
 
     render() {
+        const { loading, success } = this.state;
         return (
             <div>
                 {loading && (
                     <Message icon>
                         <Icon name="circle notched" loading />
-                        <Message.Header>Your email is being validated</Message.Header>
+                        <Message.Header>Your email is being validated.</Message.Header>
                     </Message>
                 )}
+
+                {!loading && success && <Message success icon>
+                        <Icon name="checkmark" />
+                        <Message.Content>
+                        <Message.Header>Your account has been verified.</Message.Header>
+                        <Link to="/dashboard">Go to dashboard.\</Link>
+                        </Message.Content>
+                    </Message>}
             </div>
         );
     }
