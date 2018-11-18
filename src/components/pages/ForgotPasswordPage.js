f8f6a538993e9298d587a323902ea402b8e78db5
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-18 23:41:37 +0800

    Provided code for ForgotPasswordPage.

---
+++ src/components/pages/ForgotPasswordPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ForgotPasswordPage extends from Component {
+    state= {}
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default ForgotPasswordPage;
