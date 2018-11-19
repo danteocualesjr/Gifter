5a540920ec65738742bb26a47535566d0b63f38d
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-19 10:58:37 +0800

    Imported PropTypes from prop-types and Message from semantic-ui-react in ForgotPasswordPage.

--- src/components/pages/ForgotPasswordPage.js
+++ src/components/pages/ForgotPasswordPage.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Message } from 'semantic-ui-react';
 
 class ForgotPasswordPage extends from Component {
     state= {
