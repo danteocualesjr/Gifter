10bfd5883f4838adea0d402f52f165a3577e1978
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-24 23:36:41 +0800

    Added onSubmit method.

--- src/components/forms/ResetPasswordForm.js
+++ src/components/forms/ResetPasswordForm.js
@@ -13,6 +13,16 @@ class ResetPasswordForm extends Component {
         data: { ...this.state.data, [e.target.name]: e.target.value }
     });
 
+    onSubmit = e => {
+        e.preventDefault();
+        const errors = this.validate(this.state.data);
+        this.setState({  errors });
+        if (Object.keys(errors).length === 0) {
+            this.setState({ loading: true });
+            this.props
+        }
+    }
+
     render() {
         return(
             <div>
