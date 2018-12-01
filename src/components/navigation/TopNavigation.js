c32531f6c833695eab8494a5c1e4a571dbc858c0
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-01 08:44:28 +0800

    Added Trigger in Dropdown.

--- src/components/navigation/TopNavigation.js
+++ src/components/navigation/TopNavigation.js
@@ -6,7 +6,7 @@ const TopNavigation = (props) => (
     <Menu secondary pointing>
         <Menu.Item as={Link} to="/dashboard">Dashboard</Menu.Item>
         <Menu.Menu position="right">
-            <Dropdown>
+            <Dropdown trigger={<Image avatar src={gravatarUrl(user.email)} />}>
                 <Dropdown.Item>Logout</Dropdown.Item>
             </Dropdown>
         </Menu.Menu>
