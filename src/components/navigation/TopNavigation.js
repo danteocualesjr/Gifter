2ef8d157f7c700451b1de75eb0614cbc9c9e3f15
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-01 08:40:43 +0800

    Added Links to Menu.Items.

--- src/components/navigation/TopNavigation.js
+++ src/components/navigation/TopNavigation.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
+imported { Link } from 'react-router-dom';
 
 const TopNavigation = (props) => (
     <Menu secondary pointing>
-        <Menu.Item>Dashboard</Menu.Item>
+        <Menu.Item as={Link} to="/dashboard">Dashboard</Menu.Item>
         <Menu.Menu position="right"></Menu.Menu>
     </Menu>
 );
