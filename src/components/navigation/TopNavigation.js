0ea940341bc858fd5d238acbefb71e30ddf1c618
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-01 08:38:54 +0800

    Added styling and items to Menu in TopNavigation.

--- src/components/navigation/TopNavigation.js
+++ src/components/navigation/TopNavigation.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
 
 const TopNavigation = (props) => (
-    <Menu>
-
+    <Menu secondary pointing>
+        <Menu.Item>Dashboard</Menu.Item>
+        <Menu.Menu position="right"></Menu.Menu>
     </Menu>
 );
 
