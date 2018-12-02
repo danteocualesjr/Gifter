4878f359fbd15adce7ced9e35e9c1b8fc23be394
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-02 22:58:13 +0800

    Imported connect from react-redux.

--- src/components/navigation/TopNavigation.js
+++ src/components/navigation/TopNavigation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Menu, Dropdown, Image } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import gravatarUrl from 'gravatar-url';
 
