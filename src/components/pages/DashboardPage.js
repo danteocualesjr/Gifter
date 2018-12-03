f719a75ad507fea2881f17566514e81aa97f860b
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-04 03:43:56 +0800

    Created allGiftsSelector.

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ConfirmEmailMessage from '../messages/ConfirmEmailMessage';
-import gifts from '../../reducers/gifts';
+import { allGiftsSelector } from '../../reducers/gifts';
 
 const DashboardPage = ({ isConfirmed, gifts }) => {
     return (
