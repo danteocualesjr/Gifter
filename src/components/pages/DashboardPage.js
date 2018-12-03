159fef4cecffbfdbd454634ba3129fbfb1cddbb1
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-03 15:17:13 +0800

    Configured DashboardPage.

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -2,11 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ConfirmEmailMessage from '../messages/ConfirmEmailMessage';
+import gifts from '../../reducers/gifts';
 
 const DashboardPage = ({ isConfirmed }) => {
     return (
         <div>
             {!isConfirmed && <ConfirmEmailMessage />}
+
+            {gifts.length === 0 && <AddGiftCta />}
         </div>
     )
 };
