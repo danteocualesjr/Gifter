ae6ae2f4cb8e6d4600167d1452d7a541b30cb879
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-08 09:52:14 +0800

    Added Icon component in AddGiftCtA.

--- src/components/ctas/AddGiftCtA.js
+++ src/components/ctas/AddGiftCtA.js
@@ -4,6 +4,7 @@ const AddGiftCtA = (props) => (
     <Card centered>
         <Card.Content textAlign="center">
             <Card.Header>Add new gift</Card.Header>
+            <Icon name="plus circle" size="massive" />
         </Card.Content>
     </Card>
 );
