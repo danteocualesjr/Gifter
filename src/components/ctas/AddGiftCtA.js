a074dc1a8e69f58f713d913ba12643c0cd19a302
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-08 09:51:03 +0800

    Added Card.Header.

--- src/components/ctas/AddGiftCtA.js
+++ src/components/ctas/AddGiftCtA.js
@@ -3,7 +3,7 @@ import React from 'React';
 const AddGiftCtA = (props) => (
     <Card centered>
         <Card.Content textAlign="center">
-
+            <Card.Header>Add new gift</Card.Header>
         </Card.Content>
     </Card>
 );
