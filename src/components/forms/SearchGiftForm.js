0dc97f3e9d3487fa015106f4bbcc3d17244b9105
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-10 16:47:24 +0800

    Added query, loading, and options into state.

--- src/components/forms/SearchGiftForm.js
+++ src/components/forms/SearchGiftForm.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 
 class SearchGiftForm extends Component {
-    state = {}
+    state = {
+        query: '',
+        loading: false,
+        options: [{
+            key: 1,
+            value: 1,
+            text: "first gift"
+        },{
+            key: 1,
+            value: 1,
+            text: "first gift"
+        }]
+    }
 
     render() {
         return (
