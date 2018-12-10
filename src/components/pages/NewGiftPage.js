5d524153d43b5af7f655b2728eb92a58be4b7f2b
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-10 16:40:52 +0800

    Used Segment component in NewGiftPage and imported SearchGiftForm.

--- src/components/pages/NewGiftPage.js
+++ src/components/pages/NewGiftPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchGiftForm from '../forms/SearchGiftForm';
 
 class NewGiftPage extends Component {
     state = {
@@ -7,7 +8,10 @@ class NewGiftPage extends Component {
 
     render() {
         return (
-            <div></div>
+            <Segment>
+                <h1>Add new gift</h1>
+                <SearchGiftForm />
+            </Segment>
         )
     }
 }
