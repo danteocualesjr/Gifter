62f21d6ef238f3e818fe718062178c0febc94661
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-27 23:52:55 +0800

    Added data in state.

--- src/components/forms/GiftForm.js
+++ src/components/forms/GiftForm.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 
 class GiftForm extends Component {
-    state = {}
+    state = {
+        data: {
+            giftId: this.props.gift.giftId,
+            title: this.props.gift.title,
+            authors: this.props.gift.authors,
+            cover: this.props.gift.covers[0],
+            pages: this.props.gift.pages
+        },
+    }
 
 
 }
