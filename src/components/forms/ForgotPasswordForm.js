bbb589f4bbcb5bd1d606be91aef2a389ae07723f
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-31 02:09:37 +0800

    Removed connect.

--- src/components/forms/ForgotPasswordForm.js
+++ src/components/forms/ForgotPasswordForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { Form, Button, Message } from 'semantic-ui-react';
 import isEmail from 'validator/lib/isEmail';
 import InlineError from '../messages/InlineError';
