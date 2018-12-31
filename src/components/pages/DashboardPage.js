ebfda73338266e470d3a15d75537bc7340326256
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2019-01-01 00:49:01 +0800

    Refactored DashboardPage to class component.

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ConfirmEmailMessage from '../messages/ConfirmEmailMessage';
 import { allGiftsSelector } from '../../reducers/gifts';
 import AddGiftCtA from '../ctas/AddBookCtA';
 import { fetchGifts } from '../../actions/gifts';
+
+class DashboardPage extends Component {
+    componentDidMount = () => this.onInit(this.props);
+}
 
 const DashboardPage = ({ isConfirmed, gifts }) => {
     return (
