b83db18c8ea9a8c1d356926e092f2c5add45669f
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2019-01-02 10:54:37 +0800

    Configured index.js.

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ if (localStorage.gifterJWT) {
         email: payload.email,
         confirmed: payload.confirmed
     };
+    setAuthorizationHeader(localStorage.gifterJWT);
     store.dispatch(userLoggedIn(user));
 }
 
@@ -40,4 +41,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
